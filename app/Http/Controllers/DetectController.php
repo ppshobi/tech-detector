95cb805a2a3b53886486061295c280afa18377c1
Shobi <ppshobi@gmail.com>
2016-11-20 22:30:09 +0530

    WHois working

--- app/Http/Controllers/DetectController.php
+++ app/Http/Controllers/DetectController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Novutec\DomainParser\Parser as DomainParser;
+use Novutec\WhoisParser\Parser as WhoisParser;
+
 
 class DetectController extends Controller
 {
@@ -10,7 +13,15 @@ class DetectController extends Controller
     	$this->validate($request, [
         	'domain' => 'required|max:255|url',        
     	]);
-    	
-    	dd($request->input());
+    	$domain = $request->input('domain');    	
+		$domain = preg_replace('#^https?://#', '', $domain);
+    	echo "<h4>The domain </h4>";
+    	echo $domain;
+    	$Parser = new WhoisParser('array'); 
+    	$result = $Parser->lookup($domain);
+		//echo $result->created; // get create date of domain name
+		echo "<h4>Raw Data </h4>";
+		print_r($result); // get raw output as array
+    	// dd($request->input());
     }
 }
