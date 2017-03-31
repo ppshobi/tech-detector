6874dc6fb2ea70fe7be32877e02f6f7733165fd3
Shobi <ppshobi@gmail.com>
2017-03-31 14:06:38 +0530

    Gather recent searches from UserSearch Model return a view with data

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+	
+    public function index(){
+    	$recent_searches=UserSearch::all();
+    	return view('recent_search',compact('recent_searches'));
+    }
+
+
 }
